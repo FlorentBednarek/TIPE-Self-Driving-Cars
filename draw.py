02cac608466a45d860641bd44ea124c254f8c5a3
ZRunner <z.runner.mc@gmail.com>
2020-03-07 19:05:14 +0100

    ajout des fps

--- draw.py
+++ draw.py
@@ -33,8 +33,22 @@ def rotate(car, X:list) -> list:
     (X[1]-car.position[1])*cos(radians(car.abs_rotation)) + (X[0]-car.position[0])*sin(radians(car.abs_rotation)) + car.position[1]]
 
 
-def drawvec(screen,car,angle,length) :
+def drawvec(screen: pygame.Surface, car, angle:int, length:int) :
     v = length * vector(2 * cos(radians(car.abs_rotation + angle)),
             2 * sin(radians(car.abs_rotation + angle)))
 
     pygame.draw.line(screen,car.color,(car.position[0],car.position[1]),(car.position[0]+v.x,car.position[1]+v.y),2)
+
+
+def fps(screen: pygame.Surface, font: pygame.font, clock: pygame.time.Clock):
+    # nbr = round(clock.get_fps(),3)
+    t = clock.get_rawtime()
+    nbr = 0 if t==0 else round(1000/t)
+    if nbr < 7:
+        color = (255, 0, 0)
+    elif nbr < 12:
+        color = (255, 153, 0)
+    else:
+        color = (51, 102, 0)
+    text = font.render("FPS: "+str(nbr), True, color, (255,255,255))
+    screen.blit(text, (10,5))
