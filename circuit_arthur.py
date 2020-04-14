d823d6471a6da27ec0bb27c01a86a4030c961473
ZRunner <z.runner.mc@gmail.com>
2020-04-14 19:10:27 +0200

    suppression des couleurs de debug

--- circuit_arthur.py
+++ circuit_arthur.py
@@ -116,7 +116,8 @@ def add_width(pathway: typing.List[tuple], screen_size: tuple) -> typing.List[Bo
             points_under.append((pathway[enum][0], temp))
     for path in (points_over, points_under):
         for index in range(len(path)-1):
-            color = ((index*100+70)%255, (index*90+20)%255, (index*50+40)%255)
+            # color = ((index*100+70)%255, (index*90+20)%255, (index*50+40)%255)
+            color = (0, 0, 0)
             result.append(Border(path[index], path[index+1], color))
     return result
 
@@ -158,7 +159,8 @@ def add_width_2(pathway: typing.List[tuple]) -> typing.List[Border]:
     check_angles(points_under)
     for path in (points_over, points_under):
         for index in range(len(path)-1):
-            color = ((index*100+70)%255, (index*90+20)%255, (index*50+40)%255)
+            # color = ((index*100+70)%255, (index*90+20)%255, (index*50+40)%255)
+            color = (0, 0, 0)
             if path[index][1] > SCREEN_SIZE[1]-10:
                 path[index][1] = SCREEN_SIZE[1] - 10
             elif path[index][1] < 10:
