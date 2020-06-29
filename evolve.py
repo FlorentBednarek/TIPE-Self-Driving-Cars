98e2ced807c0fe06e4664cf9f32b06026b9b3c90
Florent Bednarek <florent.bednarek@eisti.fr>
2020-06-29 21:55:51 +0200

    update

--- evolve.py
+++ evolve.py
@@ -1,132 +1,78 @@
-from NN import Network
-from classes import Car
+"""
+Fichier contenant les fonctions de mutation et d'évolution
+
+C'est dans ce fichier que l'algorithme de mutation dit "de Darwin" est défini, utilisant une
+combinaison de mutations et de mélanges pour créer une nouvelle génération de réseaux neuronaux
+à partir des meilleurs de l'ancienne génération.
+"""
+
+from classes import Car, Network
 from pygame import Color
-import random
 from copy import deepcopy as copy
-
-'''
-def Shuffle(n1,n2):
-    for j,neuron in enumerate(n1.I_layer):
-        for k,w in enumerate(neuron.weight):
-            i = random.random()
-            if i <0.5:
-                weight = random.random()*2-1
-            else :
-                w = n2.I_layer[j].weight[k]
-    for j,neuron in enumerate(n1.layer_2):
-        for w in neuron.weight:
-
-            i = random.random()
-            if i <0.5:
-                weight = random.random()*2-1
-            else :
-                w = n2.I_layer[j].weight[k]
-    for j,neuron in enumerate(n1.layer_3):
-        for w in neuron.weight:
-            i = random.random()
-            if i <0.5:
-                weight = random.random()*2-1
-            else :
-                w = n2.I_layer[j].weight[k]
-    for j,neuron in enumerate(n1.layer_4):
-        for w in neuron.weight:
-            i = random.random()
-            if i <0.5:
-                weight = random.random()*2-1
-            else :
-                w = n2.I_layer[j].weight[k]
-    for j,neuron in enumerate(n1.O_layer):
-        for w in neuron.weight:
-            i = random.random()
-            if i <0.5:
-                weight = random.random()*2-1
-            else :
-                w = n2.I_layer[j].weight[k]
-    return n1
-
-def darwin(networks):
-    #first 6 stay
-    #7-8 become half new half first 2
-    #9-10 become new
-    mutation_rate = 0.3
-    # rank = {net : net.score for net in networks}
-    # rank = {k: v for k, v in sorted(rank.items(), key=lambda item: item[1])}
-    rank = {net: net.score for net in sorted(networks, key=lambda item: item.score)}
-    print(rank)
-    rank2 = []
-    for k,v in enumerate(rank):
-        rank2.append(v)
-    rank2[0] = Network(rank2[0].car)
-    rank2[1] = Network(rank2[1].car)
-    rank2[2] = Shuffle(rank2[2],rank2[9])
-    rank2[3] = Shuffle(rank2[3],rank2[9])
-
-    for net in rank2[4:] :
-        for neuron in net.I_layer :
-            for w in neuron.weight:
-                if random.random() <mutation_rate :
-                    w = random.random() *2 -1
-        for neuron in net.layer_2 :
-            for w in neuron.weight:
-                if random.random() <mutation_rate :
-                    w = random.random() *2 -1
-        for neuron in net.layer_3 :
-            for w in neuron.weight:
-                if random.random() <mutation_rate :
-                    w = random.random() *2 -1
-        for neuron in net.layer_4 :
-            for w in neuron.weight:
-                if random.random() <mutation_rate :
-                    w = random.random() *2 -1
-        for neuron in net.O_layer :
-            for w in neuron.weight:
-                if random.random() <mutation_rate :
-                    w = random.random() *2 -1
-    return rank2
-'''
+from typing import List
+import random
 
 
-def mutation(networks):
+def mutation(networks: List[Network]) -> List[Network]:
+    """Génère une mutation sur un réseau neuronal
+
+    Chaque neurone a une faible probabilité de voir son poids modifié à une valeur aléatoire, entre
+    -2.0 et 2.0, ainsi que son bias, entre -1.0 et 1.0
+
+    Parameters
+    ----------
+    networks:
+        Liste de réseaux neuronaux à modifier
+    """
     mutation_rate = 0.15
-    for net in networks :
-        for neuron in net.I_layer :
+    for net in networks:
+        for neuron in net.I_layer:
             if random.random() < mutation_rate:
+                neuron.bias = random.random()*2 - 1
+            for i in range(len(neuron.weight)):
+                if random.random() < mutation_rate:
+                    neuron.weight[i] = random.random()*4 - 2
+        for neuron in net.layer_2:
+            if random.random() < mutation_rate:
+                neuron.bias = random.random()*2 - 1
+            for i in range(len(neuron.weight)):
+                if random.random() < mutation_rate:
+                    neuron.weight[i] = random.random()*4 - 2
+        for neuron in net.layer_3:
+            if random.random() < mutation_rate:
+                neuron.bias = random.random()*2 - 1
+            for i in range(len(neuron.weight)):
+                if random.random() < mutation_rate:
+                    neuron.weight[i] = random.random()*4 - 2
+        for neuron in net.layer_4:
+            if random.random() < mutation_rate:
+                neuron.bias = random.random()*2 - 1
+            for i in range(len(neuron.weight)):
+                if random.random() < mutation_rate:
+                    neuron.weight[i] = random.random()*4 - 2
 
 
-                  neuron.bias = random.random()*4-2
-            for i in range(len(neuron.weight)):
-                if random.random() <mutation_rate :
-                    neuron.weight[i] = random.random() *4-2
-        for neuron in net.layer_2 :
-            if random.random() < mutation_rate:
-                  neuron.bias = random.random()*4-2
-            for i in range(len(neuron.weight)):
-                if random.random() <mutation_rate :
-                    neuron.weight[i] = random.random() *4-2
-        for neuron in net.layer_3 :
-            if random.random() < mutation_rate:
-                  neuron.bias = random.random()*4-2
-            for i in range(len(neuron.weight)):
-                if random.random() <mutation_rate :
-                    neuron.weight[i] = random.random() *4-2        
-        for neuron in net.layer_4 :
-            if random.random() < mutation_rate:
-                  neuron.bias = random.random()*4-2
-            for i in range(len(neuron.weight)):
-                if random.random() <mutation_rate :
-                    neuron.weight[i] = random.random() *4-2
-        # for neuron in net.O_layer :
-        #     if random.random() < mutation_rate:
-        #           neuron.bias = random.random()*4-2
-        #     for w in neuron.weight:
-        #         if random.random() <mutation_rate :
-        #             w = random.random() *4-2
-          
-    return networks
+def swap(n1: Network, n2: Network) -> [Network, Network]:
+    """
+    Mélange les composantes de deux réseaus neuronaux
 
-def swap(n1,n2):
+    Chaque neurone du réseau 1 a 60% de chance de se faire échanger avec le neurone correspondant
+    du réseau 2.
+
+    Parameters
+    ----------
+    n1:
+        Permier réseau neuronal
+    n2:
+        Second réseau neuronal
+
+    Returns
+    -------
+    [Network, Network]:
+        Les deux réseaux une fois mélangés
+    """
     swap_rate = 0.6
-    for i,neuron in enumerate(n1.I_layer) :
+    for i, neuron in enumerate(n1.I_layer):
         if random.random() < swap_rate:
             t = neuron.bias
             neuron.bias = n2.I_layer[i].bias
@@ -134,90 +80,83 @@ def swap(n1,n2):
         for j in range(len(neuron.weight)):
             if random.random() < swap_rate:
                 n2.I_layer[i].weight[j], neuron.weight[j] = neuron.weight[j], n2.I_layer[i].weight[j]
-    for i,neuron in enumerate(n1.layer_2) :
+    for i, neuron in enumerate(n1.layer_2):
         if random.random() < swap_rate:
             t = neuron.bias
             neuron.bias = n2.layer_2[i].bias
             n2.layer_2[i].bias = t
         for j in range(len(neuron.weight)):
-            if random.random() <swap_rate :
+            if random.random() < swap_rate:
                 # t = w
                 # w = n2.layer_2[i].weight[j]
                 # n2.layer_2[i].weight[j] = t
                 n2.layer_2[i].weight[j], neuron.weight[j] = neuron.weight[j], n2.layer_2[i].weight[j]
-    for i,neuron in enumerate(n1.layer_3) :
+    for i, neuron in enumerate(n1.layer_3):
         if random.random() < swap_rate:
             t = neuron.bias
             neuron.bias = n2.layer_3[i].bias
             n2.layer_3[i].bias = t
         for j in range(len(neuron.weight)):
-            if random.random() <swap_rate :
+            if random.random() < swap_rate:
                 # t = w
                 # w = n2.layer_3[i].weight[j]
                 # n2.layer_3[i].weight[j] = t
                 n2.layer_3[i].weight[j], neuron.weight[j] = neuron.weight[j], n2.layer_3[i].weight[j]
-    for i,neuron in enumerate(n1.layer_4) :
+    for i, neuron in enumerate(n1.layer_4):
         if random.random() < swap_rate:
             t = neuron.bias
             neuron.bias = n2.layer_4[i].bias
             n2.layer_4[i].bias = t
         for j in range(len(neuron.weight)):
-            if random.random() <swap_rate :
+            if random.random() < swap_rate:
                 # t = w
                 # w = n2.layer_4[i].weight[j]
                 # n2.layer_4[i].weight[j] = t
                 n2.layer_4[i].weight[j], neuron.weight[j] = neuron.weight[j], n2.layer_4[i].weight[j]
-    # for i,neuron in enumerate(n1.O_layer) :
-    #     if random.random() < swap_rate:
-    #               t = neuron.bias
-    #               neuron.bias = n2.O_layer[i].bias
-    #               n2.O_layer[i].bias = t
-    #     for j,w in enumerate(neuron.weight):
-    #         if random.random() <swap_rate :
-    #             t = w
-    #             w = n2.O_layer[i].weight[j]
-    #             n2.O_layer[i].weight[j] = t
-    return (n1,n2)
+    return (n1, n2)
 
-def darwin2(networks):
+
+def darwin_old(networks):
     # rank = {net : net.score for net in networks}
     # rank = {k: v for k, v in sorted(rank.items(), key=lambda item: item[1])}
     # rank2 = []
-
     # for k,v in enumerate(rank):
     #     rank2.append(v)
-
-    rank2 = sorted(networks, key = lambda net: net.score, reverse=False)
-    # print([(x, x.score) for x in rank2])
+    rank2 = sorted(networks, key=lambda net: net.score, reverse=False)
     new_gen = rank2[-4:]
     for _ in range(8):
         r = random.sample(range(len(new_gen)), 2)
-        n = swap(new_gen[r[0]],new_gen[r[1]])
+        n = swap(new_gen[r[0]], new_gen[r[1]])
         new_gen.append(n[0])
         new_gen.append(n[1])
-
-
-    new_gen = mutation(new_gen)
+    mutation(new_gen)
     return new_gen
 
-def rand_color():
-    r = random.randint(0, 230)
-    g = random.randint(0, 230)
-    b = random.randint(0, 230)
-    return (r, g, b)
 
-def darwin(networks):
-    rank = sorted(networks, key=lambda net: net.score, reverse=True) # first is best
-    # print(len(rank), [x.score for x in rank])
+def darwin(networks: List[Network]) -> List[Network]:
+    """Applique le modèle d'évolution dite "de Darwin" à une population de réseaux neuronaux
+
+    Les réseaux sont triés selon leurs scores, puis les meilleurs d'entre eux sont utilisés pour
+    créer d'autres réseaux, remplaçant les moins bons. Enfin, la plus grande partie des réseaux
+    subit un phénomène de mutation altérant de manière aléatoire certaines de leurs valeurs.
+
+    Parameters
+    ----------
+    networks:
+        Liste des réseaux neuronaux sur lesquels appliquer l'évolution
+
+    Returns
+    -------
+    List[Network]:
+        Les réseaux unef fois édités
+    """
+    rank = sorted(networks, key=lambda net: net.score, reverse=True)  # first is best
     new_gen = [copy(rank[0]), copy(rank[1])]
-    for _ in range(0, max(4,len(rank)-4),2):
+    for _ in range(0, max(4, len(rank)-4), 2):
         new_gen += swap(copy(rank[0]), copy(rank[1]))
-    # print(len(new_gen))
     if len(new_gen) < len(rank):
         new_gen += [Network(copy(networks[i].car)) for i in range(len(new_gen), len(rank))]
     for x in new_gen:
-        # x.car.color = rand_color()
         x.car.abs_rotation = 0
-    new_gen[2:] = mutation(new_gen[2:])
-    # print(len(new_gen), [x.score for x in new_gen])
+    mutation(new_gen[2:])
     return new_gen[:len(networks)]
--- start.py
+++ start.py
@@ -1,109 +1,188 @@
-import draw
-from circuit_arthur import circuit_creation
-from classes import Car
-from NN import Network
-from configManager import Config, load_from_filename
-import pygame
+"""
+Fichier principal du programme.
+
+C'est celui à utiliser pour lancer le programme, qui importera tous les autres fichiers
+automatiquement.
+
+Utilisation : `python start.py`
+"""
+
 import time
-import random
+import cProfile
+import pstats
+import io
+import typing
+import copy
+from pstats import SortKey
+from math import ceil
+import pygame
+import draw
+from circuit import circuit_creation
+from classes import Car, Border, Network
+from config_manager import Config, load_from_filename
 from evolve import darwin
+from backup_manager import BackupManager
 
-vector = pygame.math.Vector2
+
+Vector = pygame.math.Vector2
 SETTINGS: Config = None
-fps = 20
+FPS = 20  # Sert à approximativement limiter les fps, sans avoir beaucoup d'impact sur les fps réels
 
 
-def rand_color():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    return (r, g, b)
+def calc_starting_pos(point_a, point_b) -> ((int, int), float):
+    """
+    Calcule la position de départ des voitures, en fonction du circuit
 
+    Parameters
+    ----------
+    point_a: (:class:`int`, :class:`int`)
+        Premier point de la bordure supérieure du circuit
+    point_b: (:class:`int`, :class:`int`)
+        Premier point de la bordure inférieure du circuit
 
-def calc_starting_pos(pointA, pointB) -> (tuple, float):
-    """Calcule la position de départ des voitures, en fonction du circuit"""
-    vect = pygame.math.Vector2(pointB[0]-pointA[0], pointB[1]-pointA[1])
+    Returns
+    -------
+    ((:class:`int`, :class:`int`), :class:`float`)
+        Les coordonnées du point de départ, et la rotation adéquate
+    """
+    vect = Vector(point_a[0]-point_b[0], point_a[1]-point_b[1])
     vect.scale_to_length(vect.length()/2)
-    new_point = pointA[0]+vect.x, pointA[1]+vect.y
-    n = pygame.math.Vector2(0, 1)
+    new_point = point_b[0]+vect.x, point_b[1]+vect.y
+    n = Vector(0, 1)
     new_angle = -round(vect.angle_to(n))
     vect = vect.rotate(-90)
     vect.scale_to_length(20)
-    new_point = new_point[0]+vect.x, new_point[1]+vect.y
+    new_point = round(new_point[0]+vect.x), round(new_point[1]+vect.y)
     return new_point, new_angle
 
 
-def manual_loop(screen: pygame.Surface, circuit: list, fps_font: pygame.font):
+def check_events() -> int:
+    """Vérifie les touches entrées par l'utilisateur
+
+    Returns
+    -------
+    int:
+        0 si rien ne se passe, 1 si la génération doit recommencer, 2 si le programme doit se
+        quitter, 3 si le programme doit se mettre en pause
+    """
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return 2
+        if event.type == pygame.KEYDOWN:
+            if event.unicode == "r":  # reset:
+                return 1
+            if event.unicode == 'p':  # pause
+                return 3
+    return 0
+
+
+def manual_loop(screen: pygame.Surface, circuit: typing.List[Border]):
+    """
+    Boucle principale pour le mode manuel du programme
+
+    Parameters
+    ----------
+    screen:
+        La fenêtre du programme
+    circuit:
+        La liste des bordures représentant le circuit
+    """
     screen_width = SETTINGS.screen_size[0]
     clock = pygame.time.Clock()
+    small_font = pygame.font.SysFont('Arial', ceil(18*SETTINGS.scale_avg))
+    title_font = pygame.font.SysFont('Arial', ceil(30*SETTINGS.scale_avg))
     dt = 1
     color = pygame.Color(SETTINGS.car_color)
-    init_pos, init_angle = calc_starting_pos(circuit["point1"], circuit["point2"])
-    car = Car(circuit["bordures"], color=color, starting_pos=init_pos,
-                abs_rotation=init_angle)
+    init_pos, init_angle = calc_starting_pos(
+        circuit["point1"], circuit["point2"])
+    car = Car(circuit["bordures"], color=color,
+              starting_pos=init_pos, abs_rotation=init_angle)
     running = True
     start_time = time.time()
 
+    on_pause = False
+
     while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
+        temp = check_events()
+        if temp == 2:
+            break
+        if temp == 3:
+            on_pause = not on_pause
 
         screen.fill(SETTINGS.colors["background"])
         draw.circuit(screen, circuit["bordures"])
         draw.car(screen, [car])
-        delta = dt * fps / 1000
+        delta = dt * FPS / 1000
 
-        pressed = pygame.key.get_pressed()
-        if pressed[SETTINGS.left_key]:
-            car.abs_rotation -= SETTINGS.car_maniability * delta
-        if pressed[SETTINGS.right_key]:
-            car.abs_rotation += SETTINGS.car_maniability * delta
-        car.apply_vector(car.direction_vector())
+        if not on_pause:
+            pressed = pygame.key.get_pressed()
+            if pressed[SETTINGS.left_key]:
+                car.abs_rotation -= SETTINGS.car_maniability * delta
+            if pressed[SETTINGS.right_key]:
+                car.abs_rotation += SETTINGS.car_maniability * delta
+            car.apply_vector(car.direction_vector())
 
-        if min(car.position) < 0:
-            car.set_position(max(car.position[0], 0), max(car.position[1], 0))
-        if max(car.position) > screen_width:
-            car.set_position(min(car.position[0], screen_width), min(
-                car.position[1], screen_width))
-        if not car.detection(screen, SETTINGS.display_rays):
-            running = False
-            print("Votre voiture a touché un mur - fin de la partie")
+            if min(car.position) < 0:
+                car.set_position(max(car.position[0], 0), max(car.position[1], 0))
+            if max(car.position) > screen_width:
+                car.set_position(min(car.position[0], screen_width), min(
+                    car.position[1], screen_width))
+            if not car.detection(screen, SETTINGS.display_rays):
+                running = False
+                print("Votre voiture a touché un mur - fin de la partie")
 
-        draw.general_stats(screen, fps_font, clock, None, None, start_time)
+        draw.general_stats(screen, small_font, clock, None, None, start_time)
+        if on_pause:
+            draw.pause_screen(screen, title_font)
+            start_time += dt/1000
         pygame.display.flip()
-        dt = clock.tick(fps)
-    
+        dt = clock.tick(FPS)
+
     for _ in range(30):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         time.sleep(0.05)
 
-def AI_loop(screen: pygame.Surface, circuit: dict, fps_font: pygame.font):
+
+def AI_loop(screen: pygame.Surface, circuit: dict) -> Network:
+    """
+    Boucle principale pour le mode automatique du programme
+
+    Le but ici est d'entraîner des IA pour qu'elles soient le plus performant possible sur le
+    circuit, donc en trouver au moins une qui arrive à la fin du circuit sans toucher aucune
+    bordure.
+
+    Parameters
+    ----------
+    screen:
+        La fenêtre du programme
+    circuit:
+        Un dictionnaire contenant la liste des bordures représentant le circuit, ainsi que les deux
+        points définissant la ligne de départ
+
+    Returns
+    -------
+    Network:
+        Le meilleur réseau de la dernière génération complétée
+    """
     print("Astuce : Appuyez sur la touche R si une voiture tourne en rond\n")
     clock = pygame.time.Clock()
+    small_font = pygame.font.SysFont('Arial', ceil(18*SETTINGS.scale_avg))
+    title_font = pygame.font.SysFont('Arial', ceil(30*SETTINGS.scale_avg))
     dt = 1
     init_pos, init_angle = calc_starting_pos(
         circuit["point1"], circuit["point2"])
-    cars = [Car(circuit["bordures"], color= SETTINGS.colors["cars"], starting_pos=init_pos,
+    cars = [Car(circuit["bordures"], color=SETTINGS.colors["cars"], starting_pos=init_pos,
                 abs_rotation=init_angle) for _ in range(SETTINGS.cars_number)]
     networks = [Network(c) for c in cars]
+    networks[0].from_json(BackupManager().load()["network"])
+    networks[0].car.color = "#00FF00"
     running = True
 
     increment = 0
-
-    def check_events() -> int:
-        # 0 - continue
-        # 1 - stop gen
-        # 2 - stop everything
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return 2
-            if event.type == pygame.KEYDOWN:
-                if event.unicode == "r":  # reset:
-                    return 1
-        return 0
+    last_sorted_networks = None
+    on_pause = False
 
     while running:
         increment += 1
@@ -115,52 +194,61 @@ def AI_loop(screen: pygame.Surface, circuit: dict, fps_font: pygame.font):
             temp = check_events()
             if temp == 1:
                 endgen = True
-                break
-            elif temp == 2:
-                # f.close()
-                return
+            if temp == 2:
+                return last_sorted_networks[0] if last_sorted_networks is not None else None
+            if temp == 3:
+                on_pause = not on_pause
 
             screen.fill(SETTINGS.colors["background"])
             draw.circuit(screen, circuit["bordures"])
             draw.car(screen, (net.car for net in networks))
 
-            # Gestion du mouvement de la voiture
-            delta = dt * fps / 1000
-            for net in networks:
-                if net.dead == False:
-                    net.update()
-                    net.car.abs_rotation += SETTINGS.car_maniability * delta * net.direction
-
-                    net.car.apply_vector(
-                        net.car.direction_vector() * net.engine * 2)
-                    if not net.car.detection(screen, SETTINGS.display_rays):
-                        net.dead = True
-                        net.car.death_time = time.time()
-
-            survived = sum(1 for n in networks if not n.dead)
-            if survived == 0:
-                endgen = True
-            elif time.time()-start_time > 10 and not cleanup_done:
+            delta = dt * FPS / 1000
+            if not on_pause:
+                # Gestion du mouvement de la voiture
                 for net in networks:
-                    if (not net.dead) and net.car.position[0] < 150:
-                        net.dead = True
-                        net.car.death_time = time.time()
+                    if not net.dead:
+                        net.update()
+                        net.car.abs_rotation += SETTINGS.car_maniability * delta * net.direction
 
-            draw.general_stats(screen, fps_font, clock,
+                        net.car.apply_vector(
+                            net.car.direction_vector() * net.engine * 2 * SETTINGS.scale_avg)
+                        if not net.car.detection(screen, SETTINGS.display_rays):
+                            net.dead = True
+                            net.car.death_time = time.time()
+
+                survived = sum(1 for n in networks if not n.dead)
+                if survived == 0:
+                    endgen = True
+                elif time.time()-start_time > 10 and not cleanup_done:
+                    for net in networks:
+                        if (not net.dead) and net.car.position[0] < 150:
+                            net.dead = True
+                            net.car.death_time = time.time()
+
+            draw.general_stats(screen, small_font, clock,
                                increment, survived, start_time)
-            draw.car_specs(screen, fps_font, networks[0])
-            draw.car_network(screen, fps_font, networks[0])
+            draw.car_specs(screen, small_font, networks[0])
+            draw.car_network(screen, small_font, networks[0])
+            if on_pause:
+                draw.pause_screen(screen, title_font)
+                elapsed = dt/1000
+                start_time += elapsed
+                for net in networks:
+                    if not net.dead:
+                        net.car.start_time += elapsed
             pygame.display.flip()
-            dt = clock.tick(fps)
+            dt = clock.tick(FPS)
 
         arrival = circuit["bordures"][-1]  # ligne d'arrivée
         # calcul des scores
         for net in networks:
             net.score = net.car.get_score()
-            if net.car.distance_to_segment(arrival.start, arrival.end) <= 8:
+            if net.car.distance_to_segment(arrival) <= 8:
                 net.score += 300  # points bonus si la voiture a atteint la ligne d'arrivée
         average = round(sum([net.score for net in networks])/len(networks))
         print(f"Génération N°{increment} terminée - score moyen : {average}")
+        last_sorted_networks = copy.deepcopy(networks)
 
         # Darwin
         networks = darwin(networks)
@@ -175,6 +263,11 @@ def AI_loop(screen: pygame.Surface, circuit: dict, fps_font: pygame.font):
 
 
 def main():
+    """
+    Fonction principale, lançant tout le programme selon la configuration donnée.
+
+    La configuration du programme est disponible dans le fichier `settings.yaml`.
+    """
     print("""Lancement du programme
 
     Assurez-vous que toutes les dépendances utilisées soient installées sur votre ordinateur ;
@@ -189,23 +282,35 @@ def main():
         print("Erreur lors du chargement de la configuration :\n"+e.args[0])
         return
 
+    # Démarrage du debug
+    if SETTINGS.debug_mode:
+        pr = cProfile.Profile()
+        pr.enable()
+
     draw.init()
     pygame.init()
     screen = pygame.display.set_mode(SETTINGS.screen_size)
-    fps_font = pygame.font.SysFont('Arial', 18)
+
     pygame.display.set_caption("TIPE")
-    border_colors = {
-        "general": SETTINGS.colors["borders"],
-        "begin": SETTINGS.colors["border-begin"],
-        "end": SETTINGS.colors["border-end"]}
-    circuit = circuit_creation(border_colors)
+    circuit = circuit_creation(SETTINGS)
 
     if SETTINGS.manual_control:
-        manual_loop(screen, circuit, fps_font)
+        manual_loop(screen, circuit)
     else:
-        AI_loop(screen, circuit, fps_font)
+        last_network = AI_loop(screen, circuit)
+        if SETTINGS.autosave:
+            BackupManager().create(network=last_network)
 
     pygame.quit()
+
+    if SETTINGS.debug_mode:
+        pr.disable()
+        s = io.StringIO()
+        sortby = SortKey.CUMULATIVE
+        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        with open("stats_report.log", "w", encoding="utf-8") as f:
+            f.write(s.getvalue())
 
 
 if __name__ == "__main__":
